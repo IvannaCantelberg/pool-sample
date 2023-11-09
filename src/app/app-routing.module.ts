79326c52d12214ffc3c52e106c02788f493e5ccc
Ivanna Cantelberg <ivannacantelberg@gmail.com>
2023-11-09 11:10:15 +0100

    routing fix for build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '/',
-    title: 'Log in',
-    data: { imageBg: false, title: 'text' },
-    loadComponent: () => import('./login-page/login/login.component').then(m => m.LoginComponent)
-  },
-  {
     path: 'sign-in',
     title: 'Sign In',
     data: { imageBg: true, title: 'text  2' },
     loadComponent: () => import('./login-page/sign-in/sign-in.component').then(m => m.SignInComponent)
-  }
+  },
+  {
+    path: '',
+    title: 'Log in',
+    data: { imageBg: false, title: 'text' },
+    loadComponent: () => import('./login-page/login/login.component').then(m => m.LoginComponent)
+  },
 
 ];
 
