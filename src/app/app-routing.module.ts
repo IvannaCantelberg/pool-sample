d0c44a67ad9f418007e33fea3bc8930cfe78a3fd
Ivanna Cantelberg <ivannacantelberg@gmail.com>
2023-11-09 11:25:30 +0100

    Working with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
     path: 'login',
     title: 'Log in',
     data: { imageBg: false, title: 'text' },
+
     loadComponent: () => import('./login-page/login/login.component').then(m => m.LoginComponent)
   },
   {
@@ -14,6 +15,7 @@ export const routes: Routes = [
     data: { imageBg: true, title: 'text  2' },
     loadComponent: () => import('./login-page/sign-in/sign-in.component').then(m => m.SignInComponent)
   },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: '**',
     redirectTo: 'login',
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
+import { AppRoutingModule, routes } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule, AppRoutingModule),
+    importProvidersFrom(BrowserModule),
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules)
+    ),
 
   ]
 })
